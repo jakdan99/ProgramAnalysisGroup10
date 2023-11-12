7c1ee1c91bb2488423d7e07acba957f2caa1084f
Dániel Jakab <jakdan99@gmail.com>
2023-11-12 14:03:23 +0100

    Interface default tests

---
+++ src/test/java/org/group9/Implement/OverrideInterfaceTest.java
@@ -0,0 +1,30 @@
+package org.group9.Implement;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OverrideInterfaceTest {
+    OverrideInterface override;
+
+    @BeforeEach
+    void setUp() {
+        override = new OverrideInterface();
+    }
+
+    @Test
+    void DefaultNoOverride() {
+        assertEquals("Default" ,override.DefaultNoOverride());
+    }
+
+    @Test
+    void defaultOverride() {
+        assertEquals("Overridden" ,override.DefaultOverride());
+    }
+
+    @Test
+    void defaultOverrideAndExtend() {
+        assertEquals("Override and Extend", override.DefaultOverrideAndExtend());
+    }
+}
